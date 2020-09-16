d695ed3d31557a89ea19afc8d2a819f0f189027b
feygon <feygon@gmail.com>
2020-09-16 10:10:24 -0700

    Successful test data

--- ETLProcess/General/GP.cs
+++ ETLProcess/General/GP.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Reflection;
 using System.Runtime.Remoting.Messaging;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using ETLProcess.General.Containers;
+using ETLProcess.General.Interfaces;
+
+using ETLProcess.General.Profiles;
 
 namespace ETLProcess.General
 {
@@ -18,7 +23,7 @@ namespace ETLProcess.General
         /// <summary>
         /// public parameterless constructor.
         /// </summary>
-        public SampleColumnTypes():base() { }
+        public SampleColumnTypes() : base() { }
         internal SampleColumnTypes(int capacity) : base(capacity) { }
         internal SampleColumnTypes(IEqualityComparer<string> comparer) : base(comparer) { }
         internal SampleColumnTypes(IDictionary<string, (Type colType, bool isKey)> dictionary) : base(dictionary) { }
@@ -31,7 +36,7 @@ namespace ETLProcess.General
     /// <summary>
     /// Alias class for string-keyed Dictionary of strings
     /// </summary>
-    public class StringMap : Dictionary<string, string> 
+    public class StringMap : Dictionary<string, string>
     {
         /// <summary>
         /// Default constructor
@@ -130,7 +135,7 @@ namespace ETLProcess.General
         public static Dictionary<T1s, T2s> Model_Select<T1s, T2s>(IList<T1s> headers, IList<T2s> row)
         {
             Model_Index_Dict<T1s, T2s> model = new Model_Index_Dict<T1s, T2s>(
-                new Index_Dictionary<T1s>(headers), 
+                new Index_Dictionary<T1s>(headers),
                 new Index_Dictionary<T2s>(row));
             Dictionary<T1s, T2s> ret = new Dictionary<T1s, T2s>();
             foreach (int i in model.Keys)
@@ -147,7 +152,7 @@ namespace ETLProcess.General
     /// </summary>
     /// <typeparam name="TClass">Type of the class to call the static method/field/property from.</typeparam>
 
-    public static class Reflection<TClass> where TClass: class
+    public static class Reflection<TClass> where TClass : class
     {
         /// <summary>
         /// A method to return an instance of type TClass, with optional parameters for its construction.
@@ -160,7 +165,7 @@ namespace ETLProcess.General
             try {
                 return (TClass)Activator.CreateInstance(type, classOptions);
             } catch (Exception err) {
-                throw new Exception($"Failure creating instance {typeof(TClass).Name}:", err); 
+                throw new Exception($"Failure creating instance {typeof(TClass).Name}:", err);
             }
         }
 
@@ -232,7 +237,7 @@ namespace ETLProcess.General
             , BindingFlags bindingMask = 0)
         {
             var instance = Activator.CreateInstance(typeof(TClass), classOptions);
-            try { 
+            try {
                 FieldInfo fieldInfo = typeof(TClass).GetField(fieldName, bindingMask);
                 return (TRet)fieldInfo.GetValue(instance);
             } catch (Exception err) {
@@ -305,4 +310,108 @@ namespace ETLProcess.General
             }
         } // end method
     } // end class
+    
+    /// <summary>
+    /// Class for extending LinQ on Dataset capabilities.
+    /// </summary>
+    public class LinQOnDataset
+    {
+        /// <summary>
+        /// Method to get the remainder of Left sans Intersection of Left/Right
+        /// </summary>
+        /// <param name="leftTable">The left table in the join</param>
+        /// <param name="leftKey">The key to the left table in the join (composite allowed, must mirror rightKey in length and types)</param>
+        /// <param name="rightTable">The right table in the join</param>
+        /// <param name="rightKey">The key to the right table in the join (composite allowed, must mirror leftKey in length and types)</param>
+        /// <returns></returns>
+        public static EnumerableRowCollection<DataRow> Left_NotInner(
+            DataTable leftTable
+            , DataColumn[] leftKey
+            , DataTable rightTable
+            , DataColumn[] rightKey)
+        {
+            EnumerableRowCollection<DataRow> query = leftTable.AsEnumerable();
+
+            // Check that all keys match, and keep those that do.
+            for (int i = 0; i < leftKey.Length; i++)
+            {
+                query = from left in query
+                        where !(from right in rightTable.AsEnumerable()
+                                select right[rightKey[i]]
+                                ).Contains(left[leftKey[i]])
+                        select left;
+
+            }
+            return query;
+        }
+        /// <summary>
+        /// Method to get the left rows of a left join of left/right.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="relation"></param>
+        /// <param name="right"></param>
+        /// <param name="columns">Optional: The columns to query, left joined with the left table (requires reformulation of new types of DataRows). Null: All columns from the left table.</param>
+        /// <typeparam name="TBasicRecordLeft"></typeparam>
+        /// <typeparam name="TBasicRecordRight"></typeparam>
+        /// <typeparam name="TProfile"></typeparam>
+        /// <returns></returns>
+        public static EnumerableRowCollection<DataRow> LeftJoin_LeftRows<TBasicRecordLeft, TBasicRecordRight, TProfile>(
+            FileDataRecords<TBasicRecordLeft, TProfile> left
+            , FileDataRecords<TBasicRecordRight, TProfile> right
+            , DataRelation relation
+            , DataColumn[] columns = null)
+            where TBasicRecordLeft : BasicRecord<TBasicRecordLeft>, IRecord<TBasicRecordLeft>, new()
+            where TBasicRecordRight : BasicRecord<TBasicRecordRight>, IRecord<TBasicRecordRight>, new()
+            where TProfile : IC_CSVFileIn<IO_FilesIn>, new()
+        {
+
+
+
+            if (columns == null) {
+                EnumerableRowCollection<DataRow> query = left.AsEnumerable();
+                query = from lt in left.AsEnumerable()
+                        join rt in right.AsEnumerable()
+                        on new {  } equals new { relation.ParentColumns }
+                        select lt;
+            } else { throw new NotImplementedException("Not impemented: 'Requires reformulation of new types of DataRows with according columns'"); }
+        }
+
+        /// <summary>
+        /// Method to get the DataSet of two left joined tables.
+        /// </summary>
+        /// <typeparam name="TBasicRecordLeft"></typeparam>
+        /// <typeparam name="TBasicRecordRight"></typeparam>
+        /// <typeparam name="TProfile"></typeparam>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <param name="relation"></param>
+        /// <returns></returns>
+        public static DataSet LeftJoin_DataSet<TBasicRecordLeft, TBasicRecordRight, TProfile>(
+            FileDataRecords<TBasicRecordLeft, TProfile> left
+            , FileDataRecords<TBasicRecordRight, TProfile> right
+            , DataRelation relation)
+            where TBasicRecordLeft : BasicRecord<TBasicRecordLeft>, IRecord<TBasicRecordLeft>, new()
+            where TBasicRecordRight : BasicRecord<TBasicRecordRight>, IRecord<TBasicRecordRight>, new()
+            where TProfile : IC_CSVFileIn<IO_FilesIn>, new()
+        {
+            var childColColl = relation.ChildColumns.AsEnumerable();
+            var parentColColl = relation.ParentColumns.AsEnumerable();
+
+            var innerOnly = left.AsEnumerable().Join(
+                right.AsEnumerable()
+                , lKey => from x in childColColl select left.Columns[x.ColumnName]
+                , rKey => from y in parentColColl select right.Columns[y.ColumnName]
+                , (lKey, rKey) => lKey).DefaultIfEmpty();
+            throw new NotImplementedException("To do: 'https://stackoverflow.com/questions/584820/how-do-you-perform-a-left-outer-join-using-linq-extension-methods' and 'https://stackoverflow.com/questions/10317117/linq-to-sql-join-on-multiple-columns-using-lambda'");
+
+                /*
+                from lt in left.AsEnumerable()
+                join rt in right.AsEnumerable()
+                on new { lt.Field(relation.ChildColumns[0].ColumnName) } equals new { } into all
+                from allX in all.DefaultIfEmpty()
+                select allX;
+            */
+
+        }
+    }
 } // end namespace
